2d5def7560f66f0a29585ff67bdb519d82b084b6
Allen Zheng <zhenga8533@gmail.com>
2024-03-08 17:54:12 -0500

    Added dynamic js loading

--- js/app.js
+++ js/app.js
@@ -6,12 +6,29 @@ $(window).on('hashchange', function(e){
 // Dynamically load pages
 $(document).ready(function() {
     // Load default content (e.g., home.html)
-    $('#main-content').load('content/pages/home.html');
+    $('#main-content').load('content/pages/home.html', function() {
+        loadJavaScript('home');
+    });
 
     // Function to load content based on clicked link
     $('.nav-link').click(function(e) {
         e.preventDefault();
         const page = $(this).attr('href');
-        $('#main-content').load(page);
+        $('#main-content').load(page, function() {
+            var mainContent = page.split('/').pop().split('.')[0]; // Extract the main content name from the URL
+            loadJavaScript(mainContent);
+        });
     });
+
+    // Function to load JavaScript specific to each main section
+    function loadJavaScript(mainContent) {
+        // Remove previously loaded script
+        $('script[src^="js/"]').remove();
+
+        if (mainContent !== 'home') {
+            var script = document.createElement('script');
+            script.src = 'js/' + mainContent + '.js';
+            document.body.appendChild(script);
+        }
+    }
 });
