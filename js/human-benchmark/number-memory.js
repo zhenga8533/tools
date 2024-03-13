8350b127317b579c1fba40597547ba79d8312cd6
Allen Zheng <zhenga8533@gmail.com>
2024-03-12 21:55:26 -0400

    created initial number display

--- js/human-benchmark/number-memory.js
+++ js/human-benchmark/number-memory.js
@@ -1,3 +1,27 @@
+let num = '';
+let level = 1;
+
 function startGame() {
-    console.log("Start!");
+    // Initialize global variables
+    num = String(Math.floor(Math.random() * 10));
+    level = 1;
+    document.getElementById('hb-btn').innerText = 'Submit';
+
+    showNumbers();
+}
+
+function showNumbers() {
+    let display = document.getElementById('message');
+    let input = document.getElementById('hb-input');
+    let btn = document.getElementById('hb-btn');
+    display.textContent = num;
+    input.disabled = true;
+    btn.disabled = true;
+
+    // Clear display after 3 seconds
+    setTimeout(() => {
+        input.disabled = false;
+        btn.disabled = false;
+        display.textContent = 'Enter the numbers in order!';
+    }, 3000);
 }
