4a7a5d3683451a1c4df3d2fb89588322599c2cd8
Allen Zheng <zhenga8533@gmail.com>
2024-03-12 22:13:27 -0400

    completed number memory general functionality

--- js/human-benchmark/number-memory.js
+++ js/human-benchmark/number-memory.js
@@ -5,12 +5,18 @@ function startGame() {
     // Initialize global variables
     num = String(Math.floor(Math.random() * 10));
     level = 1;
-    document.getElementById('hb-btn').innerText = 'Submit';
 
-    showNumbers();
+    // Update HTML elements
+    let input = document.getElementById('hb-btn');
+    input.innerText = 'Submit';
+    input.onclick = submitNumber;
+    document.getElementById('hb-input').value = '';
+    document.getElementById('message').style.color = 'black';
+
+    showNumber();
 }
 
-function showNumbers() {
+function showNumber() {
     let display = document.getElementById('message');
     let input = document.getElementById('hb-input');
     let btn = document.getElementById('hb-btn');
@@ -22,6 +28,28 @@ function showNumbers() {
     setTimeout(() => {
         input.disabled = false;
         btn.disabled = false;
-        display.textContent = 'Enter the numbers in order!';
+        display.textContent = 'Level ' + level;
     }, 3000);
+}
+
+function submitNumber() {
+    let input = document.getElementById('hb-input');
+
+    if (input.value == num) {
+        input.value = '';
+        num += String(Math.floor(Math.random() * 10));
+        level++;
+        showNumber();
+    } else endGame();
+}
+
+function endGame() {
+    let display = document.getElementById('message');
+    display.textContent = `${num} (Level ${level})`;
+    display.style.color = 'red';
+
+    // Reset hb-btn
+    let input = document.getElementById('hb-btn');
+    input.innerText = 'Start';
+    input.onclick = startGame;
 }
