289f18ac59e0ec0d69f0d2461ab6daa3996f9714
Allen Zheng <zhenga8533@gmail.com>
2024-03-19 22:09:35 -0400

    Seperated toggleStopwatch into two function

--- js/misc/stopwatch.js
+++ js/misc/stopwatch.js
@@ -1,34 +1,32 @@
 function initializeStopwatch() {
     let timer;
-    let running = false;
     let milliseconds = 0;
 
     /**
-     * Function to start or stop the stopwatch.
+     * Function to start the stopwatch.
      */
-    window.toggleStopwatch = function() {
-        if (running) {
-            clearInterval(timer);
-            document.getElementById('start-btn').style.display = '';
-            document.getElementById('stop-btn').style.display = 'none';
-        } else {
-            timer = setInterval(updateTime, 25);
-            document.getElementById('start-btn').style.display = 'none';
-            document.getElementById('stop-btn').style.display = '';
-        }
-        running = !running;
+    window.startStopwatch = function() {
+        timer = setInterval(updateTime, 25);
+        document.getElementById('start-btn').style.display = 'none';
+        document.getElementById('stop-btn').style.display = '';
+    }
+
+    /**
+     * Function to stop the stopwatch.
+     */
+    window.stopStopwatch = function() {
+        clearInterval(timer);
+        document.getElementById('start-btn').style.display = '';
+        document.getElementById('stop-btn').style.display = 'none';
     }
 
     /**
      * Function to reset the stopwatch.
      */
     window.resetStopwatch = function() {
-        clearInterval(timer);
-        running = false;
+        window.stopStopwatch();
         seconds = 0;
         milliseconds = 0;
-        document.getElementById('start-btn').style.display = '';
-        document.getElementById('stop-btn').style.display = 'none';
         document.getElementById('timer').textContent = '00h 00m 00s 000';
     }
 
