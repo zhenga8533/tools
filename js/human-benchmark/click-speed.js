b3f0489fa8241753b5499b71222c624d2b67526b
Allen Zheng <zhenga8533@gmail.com>
2024-03-26 18:20:44 -0400

    Added click speed page

---
+++ js/human-benchmark/click-speed.js
@@ -0,0 +1,9 @@
+function initializeGame() {
+    /**
+     * Starts game by resetting global variables and HTML elements.
+     */
+    window.startGame = function() {
+        
+    }
+}
+initializeGame();
